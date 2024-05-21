3f70811327c6b25238851daf8f625b0f22294319
Ashutosh <ashutosh.na.99@gmail.com>
2024-05-21 19:57:48 +0530

    init project

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  // modules: ["@hebilicious/authjs-nuxt"],
+  devtools: {
+    enabled: true,
+
+    timeline: {
+      enabled: true
+    }
+  },
+
+  // modules: ["@hebilicious/authjs-nuxt"]
+})
