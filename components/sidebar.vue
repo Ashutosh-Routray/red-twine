6ef0c681fbdf183d65773c84c0cb65387ac4ab31
Ashutosh <ashutosh.na.99@gmail.com>
2024-06-11 14:43:54 +0530

    design: add sample sidebar icons

--- components/sidebar.vue
+++ components/sidebar.vue
@@ -1,22 +1,31 @@
 <template>
   <div class="sidebarcomponent">
-    <div></div>
-    <div></div>
-    <div>e</div>
-    <div>m</div>
-    <div>i</div>
-    <div>t</div>
-    <div>e</div>
-    <div>m</div>
+    <div>
+      <AkCut class="icon" />
+    </div>
+    <div>
+      <BxPaste class="icon" />
+    </div>
+    <div>
+      <FlFilledPersonAccounts class="icon" />
+    </div>
+    <div>
+      <AnTwotoneBank class="icon" />
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-
+import { BxPaste } from "@kalimahapps/vue-icons";
+import { AkCut } from "@kalimahapps/vue-icons";
+import { FlFilledPersonAccounts } from "@kalimahapps/vue-icons";
+import { AnTwotoneBank } from "@kalimahapps/vue-icons";
 </script>
 
 <style>
-.sidebarcomponent{
+.sidebarcomponent {}
 
+.icon {
+  font-size: 2.5rem;
 }
 </style>
