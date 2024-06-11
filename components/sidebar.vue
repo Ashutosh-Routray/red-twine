40989f550caedd859cd4dd1ff1f953d1bafc1753
Ashutosh Routray <ashutosh.routray@ibm.com>
2024-06-11 13:35:23 +0530

    branches

---
+++ components/sidebar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="sidebarcomponent">
+    <div></div>
+    <div></div>
+    <div>e</div>
+    <div>m</div>
+    <div>i</div>
+    <div>t</div>
+    <div>e</div>
+    <div>m</div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+.sidebarcomponent{
+
+}
+</style>
---
+++ pages/board/[id].vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import "../../components/sidebar.vue"
+import "../../styles/sidebar.css"
+import "../../styles/board.css"
+</script>
+
+<template>
+  <div class="board">
+    <sidebar class="sidebar"></sidebar>
+  </div>
+</template>
+
+<style>
+
+</style>
